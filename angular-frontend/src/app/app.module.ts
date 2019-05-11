b91a878dfef3a499b1b3e95ec56ccd4962c7eac5
Alvin Harberts <alvin.harberts@gmail.com>
2019-05-11 15:02:37 +0200

    [INIT] - Added remaining initial frontend

---
+++ angular-frontend/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { TodoListComponent } from './todo-list.component';
+import { TodoService } from './todo.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [TodoService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
